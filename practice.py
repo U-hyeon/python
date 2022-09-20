96581731610b36dc2a31d60eb1d07b29172a55b8
U-hyeon <westwoodyousoro@gmail.com>
2022-09-20 23:12:38 +0900

    220920 python basic boolean

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+# int
 print(5)
 print(-10)
 print(3.14)
@@ -5,8 +6,16 @@ print(1000)
 print(5+3)
 print(2*8)
 print(3*(3+1))
-
+# char string
 print('풍선')
 print("나비")
 print("ㅋㅋㅋㅋㅋㅋㅋ")
 print("ㅋ"*9)
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
