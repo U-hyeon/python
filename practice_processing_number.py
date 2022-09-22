ba31f3b2cb992fa9ae624b6db4f8055a34d3498c
U-hyeon <westwoodyousoro@gmail.com>
2022-09-22 23:49:43 +0900

    220922 python basic random, num_process, quiz2

---
+++ practice_processing_number.py
@@ -0,0 +1,11 @@
+print(abs(-5)) #절대값
+print(pow(4,2)) #거듭제곱 4^2
+print(max(5,12)) #최대값
+print(min(5,12)) #최소값
+print(round(3.14)) #반올림
+print(round(4.99)) #반올림
+
+from math import * #math 라이브러리 안의 모든것을 이용한다
+print(floor(4.99)) #내림
+print(ceil(3.14)) #올림
+print(sqrt(16)) #제곱근
---
+++ practice_random.py
@@ -0,0 +1,38 @@
+from random import *
+
+print(random()) # 0.0 <= x < 1.0인 난수 x를 생성. 매번 다른값이 나온다
+print(random() * 10) #  0.0 ~ 10.0 미만의 임의의 값 생성
+print(int(random()*10)) # 0.0 ~ 10.0 미만의 임의의 정수 생성
+print(int(random()*10)) # 0.0 ~ 10.0 미만의 임의의 정수 생성
+print(int(random()*10)) # 0.0 ~ 10.0 미만의 임의의 정수 생성
+print(int(random()*10) + 1) # 1.0 ~ 10.0 이하의 임의의 정수 생성
+print(int(random()*10) + 1) # 1.0 ~ 10.0 이하의 임의의 정수 생성
+print(int(random()*10) + 1) # 1.0 ~ 10.0 이하의 임의의 정수 생성
+print(int(random()*10) + 1) # 1.0 ~ 10.0 이하의 임의의 정수 생성
+print(int(random()*10) + 1) # 1.0 ~ 10.0 이하의 임의의 정수 생성
+print(int(random()*10) + 1) # 1.0 ~ 10.0 이하의 임의의 정수 생성
+print(int(random()*10) + 1) # 1.0 ~ 10.0 이하의 임의의 정수 생성
+
+print("random")
+print(int(random() * 45) + 1) # 1 ~ 45이하
+print(int(random() * 45) + 1) # 1 ~ 45이하
+print(int(random() * 45) + 1) # 1 ~ 45이하
+print(int(random() * 45) + 1) # 1 ~ 45이하
+print(int(random() * 45) + 1) # 1 ~ 45이하
+print(int(random() * 45) + 1) # 1 ~ 45이하
+
+print("randrange")
+print(randrange(1,46)) # 1~46 미만의 임의의 정수값
+print(randrange(1,46)) # 1~46 미만의 임의의 정수값
+print(randrange(1,46)) # 1~46 미만의 임의의 정수값
+print(randrange(1,46)) # 1~46 미만의 임의의 정수값
+print(randrange(1,46)) # 1~46 미만의 임의의 정수값
+print(randrange(1,46)) # 1~46 미만의 임의의 정수값
+
+print("randint")
+print(randint(1,45)) # 1~45 이하의 임의의 정수값
+print(randint(1,45)) # 1~45 이하의 임의의 정수값
+print(randint(1,45)) # 1~45 이하의 임의의 정수값
+print(randint(1,45)) # 1~45 이하의 임의의 정수값
+print(randint(1,45)) # 1~45 이하의 임의의 정수값
+print(randint(1,45)) # 1~45 이하의 임의의 정수값
