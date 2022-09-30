15a6e1e6479fc56318a53c81cc269a9f622119a8
U-hyeon <westwoodyousoro@gmail.com>
2022-09-30 23:38:26 +0900

    220930 python basic tuple

---
+++ practice_tuple.py
@@ -0,0 +1,16 @@
+# 속도가 list 보다 훨씬 빠르다. 내용 변경/추가 불가
+
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# menu.add("생선까스") #불가함
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+# name, age, hobby = "김종국", 20, "코딩"
+(name, age, hobby) = ("김종국", 20, "코딩") #위와 같음
+print(name, age, hobby)
